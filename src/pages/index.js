4bad9566a7325abcafb7c8f6d4e2df57487da34f
Sriaditya Vedantam <sriaditya.vedantam@gmail.com>
2023-12-09 21:12:17 -0500

    fix img

--- src/pages/index.js
+++ src/pages/index.js
@@ -118,7 +118,7 @@ class Home extends React.Component {
                                                 </div>
                                             </div>
                                             <div className="col-md-5">
-                                                <StaticImage width={1024} height={768} className="img-fluid main-rounded-end-lg" src="https://zyphensvc.com/media/cyberdog.jpg" alt="Egg Heads CP-XIV Team" />
+                                                <StaticImage width={1024} height={768} className="img-fluid main-rounded-end-lg" src="../img/cypatxiii.jpg" alt="Egg Heads CP-XIV Team" />
                                             </div>
                                         </div>
                                     </div>
