6a3dc120d77c9ffc9eb1ec1657d948ca71402da7
Sriaditya Vedantam <sriaditya.vedantam@gmail.com>
2025-11-29 12:10:07 -0500

    asked for my key

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,6 +17,7 @@ class Contact extends React.Component {
                                 <h1 className="main-reveal-text-short mt-0">Contact</h1>
                                 <div className="main-fade-in-animation">
                                     <p>You can send an email to <code class="language-text">svedantam at zyphensvc dot com</code>.</p>
+                                    <p>Here is my <a aria-label="PGP key" href="/media/Zyphen.asc">PGP key</a> if you wish to communicate using encryption.</p>
                                     <p>My Discord tag is <a target="_blank" rel="noopener noreferrer" href="https://discord.com/">ZyphenSVC</a>.</p>
                                     <p>I can also be found on social media @svedantam or @ZyphenSVC.</p>
                                 </div>
