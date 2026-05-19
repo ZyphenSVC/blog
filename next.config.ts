a52b20ac1bbfba1bf0f679dfd535321c4fec7fcd
Sriaditya Vedantam <sriaditya.vedantam@gmail.com>
2026-05-19 11:04:30 -0400

    pages optimization

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
